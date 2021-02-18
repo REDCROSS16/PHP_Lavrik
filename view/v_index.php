1bd8bb8bb9c06e589d4fb47dd6db4df69168d8c4
redcross16 <belkill@mail.ru>
2021-02-19 00:39:32 +0300

    добавлен bootstrap

--- view/v_index.php
+++ view/v_index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Мой сайт на PHP</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+    
+</body>
+</html>
 <h1> CHAT </h1>
 <a href='index.php?c=add'>Добавить сообщение</a>
 <a href='index.php?view=table'>Отобразить как таблицу</a>
