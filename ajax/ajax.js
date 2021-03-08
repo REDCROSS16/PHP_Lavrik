06df0725666590621646f688ff58ef7320e6c6ab
redcross16 <belkill@mail.ru>
2021-03-08 23:48:34 +0300

    Ajax training

---
+++ ajax/ajax.js
@@ -0,0 +1,14 @@
+let result = 0;
+let button = document.getElementById('button');
+// let result = document.getElementById('result');
+// let button = 
+
+let math = 0;
+
+math = 4 * 4 * 4;
+
+result.innerHTML = math;
+
+button.addEventListener('click', function () {
+    let promise = fetch('ajax.php');
+});
---
+++ ajax/client.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<title>Ajax training page</title>
+    	<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    	<link rel="stylesheet" href="../assets/css/style.css">
+		
+	</head>
+	<body>
+		
+	<div class="row">	
+	<div class="container col-9">
+		<p>Ajax отправка запроса</p>
+		<div id="result"></div>
+			<div>
+				<input type="submit" id="button">
+			</div>
+		</div>
+		
+	</div>
+    <script src="ajax.js"></script>
+	</body>
+</html>
