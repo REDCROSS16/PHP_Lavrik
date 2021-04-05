bbbcdad3250d8ae6e8dbab1bf085c11e07f5b099
redcross16 <belkill@mail.ru>
2021-04-06 01:24:26 +0300

    exercise 16.1

--- classes/Arr.php
+++ classes/Arr.php
@@ -2,14 +2,25 @@
 
 class Arr 
 {
-    public $numbers = [];
+    private $numbers = [];
 
-    public function add (array $nums) {
-        foreach ($nums as $num) {
-            array_push($this->numbers, $num);    
+    public function __construct($nums)
+    {
+        $this->numbers = $nums;
+    }
+
+    public function add ($nums) {
+        if (is_array($nums)) {
+            foreach ($nums as $num) {
+                array_push($this->numbers, $num);    
+            }
+            print_r($this->numbers);
+            return $this->numbers;
+        } 
+        if (is_numeric($nums)) {
+            return array_push($this->numbers, $nums);
         }
-        print_r($this->numbers);
-        return $this->numbers;
+        
     }
 
     public function getAvg (array $nums) {
@@ -25,15 +36,30 @@ class Arr
         return "Среднее арифмитическое чисел : $ariphmetic";
     }
 
+    public function getSum() {
+        return array_sum($this->numbers);
+    }
+
 }
 
 
-$out = new Arr;
+// $out = new Arr;
 
-$array = [1,2,3,4,5, 50];
-echo '<pre>';
-$out->add($array);
-echo '</pre>';
+// $array = [1,2,3,4,5, 50];
+// echo '<pre>';
+// $out->add($array);
+// echo '</pre>';
+
+// echo '<br>';
+// echo $out->getAvg($array);
+
+$arr = new Arr([1, 2, 3, 5, 7]);
+$arr->add(10);
+$arr->add(20);
+
+echo '<br> сумма чисел ' . $arr->getSum() . '<br>';
+echo 'Сумма двух методов объектов: ';
+echo (new Arr([50, 100, 200]))->getSum() + (new Arr([30, 50 , 100]))->getSum(); 
 
 echo '<br>';
-echo $out->getAvg($array);
+echo '16.1 : ' . (new Arr([300,300,300,100]))->getSum();
