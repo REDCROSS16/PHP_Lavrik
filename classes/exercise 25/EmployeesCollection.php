c0d31978fa36f3aa83ced13fcd5cafc978623657
redcross16 <belkill@mail.ru>
2021-04-16 02:17:37 +0300

    Exercise 25

---
+++ classes/exercise 25/EmployeesCollection.php
@@ -0,0 +1,26 @@
+<?php 
+require_once 'Employee.php';
+class EmployeesCollection 
+{
+    private $employees = [];
+
+    public function add ($employee) 
+    {
+        $this->employees[] = $employee;
+    }
+    public function getTotalSalary () {
+        $sum = 0;
+        foreach ($this->employees as $employee) {
+            $sum += $employee->getSalary();
+        }
+        return $sum;
+    }
+}
+
+$employeesCollection = new EmployeesCollection;
+$employeesCollection->add(new Employee('Коля', 2000));
+$employeesCollection->add(new Employee('Ваня', 3000));
+$employeesCollection->add(new Employee('Саша', 2000));
+$employeesCollection->add(new Employee('Петя', 3500));
+
+echo $employeesCollection->getTotalSalary();
