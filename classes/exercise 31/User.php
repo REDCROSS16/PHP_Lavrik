e03881969ee179ccacf45534c0e0d089786f9a1b
redcross16 <belkill@mail.ru>
2021-04-25 18:34:17 +0300

    Upd message / add PHP storm

--- classes/exercise 31/User.php
+++ classes/exercise 31/User.php
@@ -1,6 +1,24 @@
 <?php
 
-class User 
+class User31
 {
-    
+    public $name;
+    private static $count;
+
+    public function __construct ($name) {
+        $this->name = $name;
+        self::$count++;
+    }
+    public static function getCount(){
+        return self::$count;
+    }
 }
+
+$test1 = new User31('va');
+$test2 = new User31('asd');
+$test3 = new User31('asd');
+$test4 = new User31('asd');
+
+
+echo $test2::getCount();
+echo User31::getCount();
