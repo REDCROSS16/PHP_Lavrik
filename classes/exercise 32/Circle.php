e66188c9732e33d8877ec28a73f841b4ad3d79e6
redcross16 <belkill@mail.ru>
2021-04-26 00:22:21 +0300

    exercise 33

--- classes/exercise 32/Circle.php
+++ classes/exercise 32/Circle.php
@@ -1,6 +1,7 @@
 <?php
-
+require_once 'Test.php';
 class Circle
+
 {
     const PI = 3.14;
     private $radius;
@@ -21,6 +22,35 @@ class Circle
 }
 
 
-$circle = new Circle(20);
-echo $circle->getCircuit();
-echo $circle->getSquare();
+$circle = new Circle(10);
+echo 'Окружность: ' . $circle->getCircuit();
+echo '<br>';
+echo 'Площадь: ' . $circle->getSquare();
+
+echo get_class($circle); # получить класс объекта
+print_r (get_class_methods($circle)); # получить методы класса
+
+$test = new Test2;
+$arr = get_class_methods($test);
+$test->name = 'alex';
+foreach ($arr as $item) {
+    echo $test->$item() . '<br>';
+}
+
+
+$array = get_class_vars('Test');
+print_r($array);
+$out = '';
+//foreach ($array as $arr) {
+//    $out .= $arr . '<br>';
+//}
+//
+//echo  $out;
+
+print_r( $test->getVars());
+
+echo '<hr>';
+echo class_exists(get_class($test));
+echo class_exists('Test2');
+echo '<hr>';
+echo 'есть ли класс ' . $_GET['class'].'? - ' . class_exists($_GET['class']);
--- classes/exercise 32/Test.php
+++ classes/exercise 32/Test.php
@@ -3,15 +3,22 @@
 class Test2
 {
     const CONSTANT = 'test';
+    public $name;
 
     public function getConstant(){
         return self::CONSTANT;
     }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getVars() {
+        return get_class_vars(get_class($this));
+    }
 }
 
-
-echo Test2::CONSTANT;
-
-$test32 = new Test2;
-
-echo $test32->getConstant();
