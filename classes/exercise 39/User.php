a40accd49ba02ebe0b8fae7ecb39fa438742394b
redcross16 <belkill@mail.ru>
2021-05-03 00:01:20 +0300

    exercise 39 complete

---
+++ classes/exercise 39/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace exercise_39;
+
+class User implements iUser 
+{
+    private $name;
+    private $age;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
---
+++ classes/exercise 39/iCube.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace exercise_39;
+
+interface iCube 
+{
+    public function __construct($a);
+    public function cubeSquare();
+    public function cubeVolume();
+}
