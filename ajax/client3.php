64b77b64fdbc158e282ace832cebfbabc4413695
redcross16 <belkill@mail.ru>
2021-05-28 02:09:53 +0300

    exercise 58 реализация класса по интерфейсу iFile and AJAX

--- ajax/client3.php
+++ ajax/client3.php
@@ -8,7 +8,21 @@
     <title>Lesson 3</title>
 </head>
 <body>
+    <p>
+        Пусть сервер ожидает три GET параметра с числами, а возвращает сумму этих чисел.
+        На клиенте сделайте HTML форму для ввода этих чисел. Сделайте так, чтобы данные формы отправлялись
+        через AJAX. После получения ответа сервера запишите этот ответ в какой-нибудь абзац.
+    </p>
+    <form action="" method="GET" id="form">
+        <input type="text" name="num1">
+        <input type="text" name="num2">
+        <input type="text" name="num3">
+        <input type="submit">
+    </form>
 
+
+    <p id="result"></p>
+    <script src="ajax3.js"></script>
 </body>
 </html>
 
