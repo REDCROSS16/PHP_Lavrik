d098a8130967876f457e78648e08a1bcc77db316
redcross16 <belkill@mail.ru>
2021-06-07 23:39:54 +0300

    array training

--- classes/exercise 24/array.php
+++ classes/exercise 24/array.php
@@ -15,3 +15,36 @@ for($i=0;$i<= 10; $i++) {
 }
 
 print_r($arr2);
+
+$array = [
+    ['href' => '1.html', 'text' => 'ссылка 1'],
+    ['href' => '2.html', 'text' => 'ссылка 2'],
+    ['href' => '3.html', 'text' => 'ссылка 3'],
+];
+
+$out = '<ul>';
+foreach ($array as $item) {
+    $out .= '<li><a href="' . $item['href'] . '">' . $item['text'] . '</a></li>';
+}
+$out .= '</ul>';
+
+echo $out;
+
+$users = [
+    ['name' => 'Коля', 'age' => '30', 'salary' => '500'],
+    ['name' => 'Вася', 'age' => '31', 'salary' => '600'],
+    ['name' => 'Петя', 'age' => '32', 'salary' => '700'],
+];
+
+$out2 = '<table>';
+foreach ($users as $user) {
+    $out2 .= '<tr>'
+        . '<td>' . $user['name'] . '</td>'
+        . '<td>' . $user['age'] . '</td>'
+        . '<td>' . $user['salary'] . '</td>'
+        . '</tr>';
+}
+
+$out2 .= '</table>';
+
+echo $out2;
