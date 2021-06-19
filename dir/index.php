41560cfbf6f3341a461dac41c12b0f3d62d91995
redcross16 <belkill@mail.ru>
2021-06-19 20:00:26 +0300

    htaccess

--- dir/index.php
+++ dir/index.php
@@ -27,8 +27,25 @@
 
 //Пусть в корне вашего сайта лежит файл test.txt, в котором записано некоторое число.
 //Откройте этот файл, возведите число в квадрат и запишите обратно в файл.
-$num = file_get_contents('test.txt');
-$num *= $num;
-file_put_contents('test.txt', $num);
+//$num = file_get_contents('test.txt');
+//$num *= $num;
+//file_put_contents('test.txt', $num);
+//echo file_get_contents('test.txt');
 
-echo file_get_contents('test.txt');
+//Пусть в корне вашего сайта лежит файл count.txt. Изначально в нем записано число 0.
+//Сделайте так, чтобы при обновлении страницы наш скрипт каждый раз увеличивал
+//это число на 1. То есть у нас получится счетчик обновления страницы в виде файла.
+//$count = file_get_contents('count.txt');
+//file_put_contents('count.txt', ++$count );
+//echo file_get_contents('count.txt');
+
+//Пусть в корне вашего сайта лежат файлы 1.txt, 2.txt и 3.txt. Вручную сделайте массив с именами этих файлов.
+//Переберите его циклом, прочитайте содержимое каждого из файлов,
+//слейте его в одну строку и запишите в новый файл new.txt. Изначально этого файла не должно быть.
+$arr = ['1.txt', '2.txt', '3.txt'];
+$result = '';
+foreach ($arr as $file) {
+    $result .= file_get_contents($file);
+}
+
+echo $result;
