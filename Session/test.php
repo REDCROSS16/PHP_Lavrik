6ab794496b828e998f61beb9950ddfda23f11e4d
redcross16 <belkill@mail.ru>
2021-06-29 00:37:48 +0300

    session training

--- Session/test.php
+++ Session/test.php
@@ -1,5 +1,8 @@
 <?php
 
 session_start();
-echo $_SESSION['name'] . '<br>' . $_SESSION['surname'] . '<br>';
+//echo $_SESSION['name'] . '<br>' . $_SESSION['surname'] . '<br>';
 var_dump($_SESSION);
+//var_dump($_SESSION['name']);
+if (isset($_SESSION['country'])) var_dump($_SESSION['country']);
+echo $_SESSION['country'];
